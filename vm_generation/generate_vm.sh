88cf80e7b321e0bea9014db1e1c2187bc6978a86
Miguel Angel Munoz Gonzalez <magonzalez@fortinet.com>
2017-07-11 12:22:13 +0200

    Scripts for startup on firstboot in VM - More stability

--- vm_generation/generate_vm.sh
+++ vm_generation/generate_vm.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#************************************************
+# Generate VM with Netconf Proxy
+#
+# Use: ./generate_vm
+#
+# Output will be:
+#    delivery.img
+#    delivery-cidata.img
+#
+# Miguel Angel Muñoz Gonzalez
+# magonzalez(at)fortinet.com
+#
+#************************************************
 
 set -x
 
@@ -14,12 +27,13 @@ rm -f snmp_netconf.img
 rm -f snmp_netconf-cidata.iso
 rm -f user-data
 rm -f meta-data
+rm -f install_script
 rm -f delivery.img
 rm -f delivery-cidata.iso
 
-# *******************************
+#************************************************
 # Get Ubuntu 16.04 as base image
-# *******************************
+#************************************************
 
 if [ ! -e ${UBUNTU_IMAGE_NAME} ]; then
 
@@ -32,32 +46,20 @@ if [ ! -e ${UBUNTU_IMAGE_NAME} ]; then
 fi
 cp ${UBUNTU_IMAGE_NAME} snmp_netconf.img
 
-# *******************************
-# Install
-# *******************************
-
-#sudo guestfish -a snmp_netconf.img --rw -i upload ../service/snmp-netconf.conf.old /etc/service/snmp-netconf.conf.old
-#sudo guestfish -a snmp_netconf.img --rw -i upload ../netconf-gw.py /opt/netconf-gw.py
-#sudo guestfish -a snmp_netconf.img --rw -i upload ../netconf-gw.py /opt/netconf-gw.py
-
+#************************************************
+# Copy netconf server code to image
+#************************************************
 
 mkdir -p /tmp/guest_snmp/
 sudo guestmount /tmp/guest_snmp/ -a snmp_netconf.img -m /dev/sda1
 
-
 sudo rsync -r -v --max-size=32768 ../*  /tmp/guest_snmp/opt/
 
-
-
-#sudo cp -r ../* /tmp/guest_snmp/opt/
-#sudo cp ../service/snmp-netconf.conf.old /tmp/guest_snmp/etc/service/
 sudo guestunmount /tmp/guest_snmp/
 
-
-
-# *******************************
-# Test the VM
-# *******************************
+#************************************************
+# Generate Cloud Init for VM
+#************************************************
 
 cat >meta-data <<EOF
 instance-id: snmp_netconf
@@ -76,13 +78,25 @@ users:
     sudo: ALL=(ALL) NOPASSWD:ALL
 EOF
 
+
+#************************************************
+# Start VM and install additional stuff
+#************************************************
+
 rm -rf snmp_netconf-cidata.iso
 genisoimage -output snmp_netconf-cidata.iso -volid cidata -joliet -rock user-data meta-data
 
-sudo virt-sysprep -a snmp_netconf.img --hostname snmp_netconf --root-password password:m \
-    --firstboot-command 'sudo apt-get update ; sudo apt-get install -y python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev ; sudo pip install paramiko pysnmp lxml; cp /opt/service/snmp-netconf.service /lib/systemd/system/snmp-netconf.service; systemctl enable snmp-netconf; systemctl start snmp-netconf'
+cat >install_script << EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev
+sudo pip install paramiko pysnmp lxml
+cp /opt/service/snmp-netconf.service /lib/systemd/system/snmp-netconf.service
+systemctl enable snmp-netconf
+systemctl start snmp-netconf
+EOF
 
-#;sudo apt-get update --fix-missing;sudo apt-get install -y python-pip python-lxml python-paramiko;sudo pip install pysnmp'
+sudo virt-sysprep -a snmp_netconf.img --hostname snmp_netconf --root-password password:m --firstboot install_script
 
 virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name snmp_netconf --disk snmp_netconf-cidata.iso,device=cdrom --ram 2048 --vcpus 1 --disk snmp_netconf.img,size=3 --network network=default,mac="08:00:27:4c:10:10"
 
@@ -90,7 +104,7 @@ virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared
 host_ip=$(virsh net-dhcp-leases default|grep "08:00:27:4c:10:10" |egrep -Eo "([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*)")
 
 echo "Scanning port 830 on server:"
-until ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $host_ip sudo netstat -puntax|grep 830
+until ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $host_ip sudo netstat -puntax|grep ":::830"
 do
   sleep 1
   echo -ne "."
@@ -105,15 +119,30 @@ virsh destroy snmp_netconf
 sleep 20
 
 cp snmp_netconf.img delivery.img
-cp snmp_netconf-cidata.iso delivery-cidata.iso
+genisoimage -output delivery-cidata.iso -volid cidata -joliet -rock user-data meta-data
+
+
+cat >install_script_for_delivery <<EOF
+useradd -m -p "" vagrant
+chage -d 0 vagrant
+ssh-keygen -A
+rm -rf /var/lib/cloud/*
+cloud-init init
+EOF
 
 sudo virt-sysprep -a delivery.img --root-password password:m \
     --delete /var/lib/cloud/* \
-    --firstboot-command 'useradd -m -p "" vagrant ; chage -d 0 vagrant; ssh-keygen -A; rm -rf /var/lib/cloud/*; cloud-service service'
-
+    --firstboot install_script_for_delivery
 
 exit 0
 
+#************************************************
+# Bonus: Run this to test VM
+# Note when you run it first time it will not
+# be usable for other system/vim
+#************************************************
+
+virsh undefine snmp_netconf
 virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name delivery --ram 2048 --vcpus 1  --disk delivery.img,size=3 --disk delivery-cidata.iso,device=cdrom --network network=default,mac="08:00:27:4c:10:10"
 
 
